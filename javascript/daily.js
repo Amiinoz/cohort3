0a4a6dde033a2487d32cc3cd1de45723e4fcd43b
Amiin <mohamedmagan@Amiins-MacBook-Pro.local>
2019-10-15 12:49:41 -0600

    15-10-2019

---
+++ javascript/daily.js
@@ -0,0 +1,55 @@
+
+
+// Daily exercise 1
+// Write a function that will receive two parameters. The function will compare the parameters and do the following
+// . if the two parameters are ewual. log nothing and return true
+// . if the two parameters are not equal. log to the console the following and return false:
+// "p1->"parameter 1
+// "p2->"parameter 2
+
+
+// 1. is p1 = p2 return true
+// 2. if p1 != p2 console.log("p1->"parameter 1 ,"p2->"parameter 2);
+// Solution is to use the Ternanry operator
+//condition? p1: p2;
+ 
+// var (p1-->)= a;
+// var (p2--> )= b;
+// var assertEquals = (a === b)?"true":console.log( "the two values are not the same” + a  + b);
+
+
+// Daily exercise 2
+// copy and paste this code into ‘daily.test.js’
+// Create array of name which consist of first and last name
+// 
+// ?
+
+
+// var makeEmailArr = (name) => {
+//     var fullName = ["first", "last"];
+//     var email = ["@evolveu.ca"];
+    
+
+//     // console.log(name + eamil);
+//     return name[0][0] + name + email;
+
+
+// }
+
+// Daily Exercise 3
+// var name = {
+//     fname: 'first',
+//     lname: 'last',
+// };
+
+
+// var makeEmailObj = name.map(function('first','last'){
+
+//     return first;
+// });
+
+
+
+// console.log(makeEmailObj);
+
+
---
+++ javascript/daily.test.js
@@ -0,0 +1,11 @@
+test('email builder from an array', () => {
+    const name = ["first", "last"];
+    expect(functions.makeEmailArr(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["First", "Last"]))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["Bill", "Smith"]))
+        .toEqual("bill.smith@evolveu.ca");
+});
+
+
