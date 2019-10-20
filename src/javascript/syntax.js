68c2dbd46aed01e81157b85d95283887b9dddbff
Amiin <mohamedmagan@Amiins-MacBook-Pro.local>
2019-10-20 15:13:34 -0600

    Working on comp 100 and dailies

--- src/javascript/syntax.js
+++ src/javascript/syntax.js
@@ -1,36 +1,117 @@
-TODO:
-// define attributes / variables
 
+
+const functions = {
 
 // number
+multiplyNumbers: () =>{
+    let a = 2;
+    let b = 2;
+    if( a * b <= 8){
+        return (a * b);
+    }
+},
 // string
+stringName: () => {
+let fname = "John";
+let massage = fname.concat(" " +"Is a teatcher in our school");
+return (massage);
+},
 // boolean
-// array
+boolean: (a, b) => { 
+        return a !== b; 
+},
+// array 
+bestCars: () => {
+    const cars = ['Toyota','Ford','BMW'];
+    cars.splice(0,2);
+    return (cars);
+},
 // dictionary / objects
+custInfo: (info) => {
+    switch (info) {
+        case 'name':
+          return ['Mike', 'Bill'];
+        case 'Age':
+          return [34, 23];
+        case 'AccNumber':
+          return [237702, 33400];
+        default:
+          return (237702);
+        //   return ('AccNumber'[0]);
+      }
+ },
 // undefined
-
-
-
+returnUndefined: () =>{
+   const carsModels = {
+        names: [ 'ford', 'Honda', 'Fiat'],
+        models: ['Focus', 'Civic', 'Oto'],
+        makeYear: [2013, 2019, 1978],
+   }
+   if(typeof carsModels[2] > 2020){
+       return (undefined);
+   }
+},
 // sample if / else
-TODO:
+usingIfElse: () => {
+    const x = 12;
+    const y = 10;
+    const z = x + y;
+
+    if( x < y){
+        console.log("x is greater then y")
+    }else{
+        return (z);
+    }
+},
 // functions
+buildingsWeOwn: () => {
+    let residentials = [
+        type = ('houses', 'condos', 'appartments'),
+        rental = (500, 1400, 300)];
+
+    if ( buildingsWeOwn === residentials.type[0] && residentials.rental < 300){
+        console.log(" Sorry we do not hava any houses available for rent");
+    }else{
+        console.log("What can of rental are you looking for");
+    }
+},
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+//TODO:
+
 // parameters
 // returns
 
-TODO:
+//TODO:
 // arrays
 // add to the front
 // add to the end
 // update values
 
-TODO:
+//TODO:
 // loops 
 // for
 // for/in
 // while
 // do while
 // forEach (with array and function)
-TODO:
+//TODO:
 // Objects / Dictionaries
 // declare object
 // lookup key to retrieve value
+// };
+
+export default functions;
--- src/javascript/syntax.test.js
+++ src/javascript/syntax.test.js
@@ -1,9 +1,25 @@
 import functions from './syntax'
 
-
-
-
-test('Check if variable type a number' , () =>{
-    expect(functions.var(8)).toBe(4);
+test('Check if variable return is number 4' , () =>{
+    expect(functions.multiplyNumbers(8)).toBe(4);
 });
+test('Check if variable returns string' , () =>{
+    expect(functions.stringName()).toBe("John Is a teatcher in our school");
+});
+test('Check if the boolean returns false' , () =>{
+    expect(functions.boolean()).toBe(false);
+});
+test('Using splice method on Arrays' , () =>{
+    expect(functions.bestCars()).toEqual(['BMW']);
+});
+test('Using dictionary, object' , () =>{
+    expect(functions.custInfo()).toEqual(237702); 
+});
+test('Using undefined' , () =>{
+    expect(functions.returnUndefined()).toEqual(undefined); 
+});
+test('Using if else statement' , () =>{
+    expect(functions.buildingsWeOwn()).toEqual("What can of rental are you looking for"); 
+});
+
 
