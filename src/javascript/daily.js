491f0efa83c902732dcf66c5cd8dd4c3e97cb0d7
Amiin <mohamedmagan@Amiins-MacBook-Pro.local>
2019-10-24 08:10:58 -0600

    daily's

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -4,85 +4,59 @@ import {
 
 const functions = {
 
-
-    
-    
-    // Advanced Arrays forEach
-//     makeForEach: () => {
-//     const array = [2,4,20,30];
-//     const double = [];
-//     const newArray = array.forEach((num) => {
-//         double.push(num*2);
-//         console.log(double);
-//     })
-// },
-
-
-
-    // for of loop
-     //TODO:
-    //  makeForOfLoop: () => {
-    //     let cowSays =  "moo";
-    //     for (let value of cowSays){
-    //         console.log(value);
-    //     }
-    //  },
-    //FIXME:
-    // for in loop
-     makeForInLoop: (object1) =>{
-        const person = {
-            name:"Mark",
+    makeForOfLoop: (arr) => {
+        const airLines1 = ['United', 'WestJet', 'AirCanada'];
+        let item;
+        for (item of airLines1) {
+            return item;
+        }
+    },
+    makeForInLoop: (obj) => {
+        let person = {
+            name: "Mark",
             Age: 23,
             Occupation: "Lawyer"
         };
-         
-         for (let item in person.name){
-            return(person.Occupation);
+        let personalInfo = "";
+        let i;
+        for (i in person) {
+            personalInfo += person[i] + "";
+        };
+        return personalInfo;
 
-         }
-     },
-    // do while loop
+    },
     makeDoWhileLoop: () => {
         let counter = 5;
-        do{
-            return(counter);
-                counter ++;
-            }
-            while(counter > 0)
+        do {
+            return (counter);
+            counter++;
+        }
+        while (counter > 0)
     },
-    // while loop
     makeBasicWhileLoop: () => {
         let nums = 6;
-        while(nums < 7){
-           return nums;
-           nums++;
+        while (nums < 7) {
+            return nums;
+            nums++;
         }
     },
-  
-    // Forloop
-    makeBasicLoop: () =>{
-         let array1 =[];
-         for( let i = 0; i <= 6; i++){
-             array1.push(i);
-         }
-         return array1;
+    makeBasicLoop: () => {
+        let array1 = [];
+        for (let i = 0; i <= 6; i++) {
+            array1.push(i);
+        }
+        return array1;
     },
-
-    // makeEmailObj
     makeEmailObj: (obj) => {
         return (obj.fname.toLowerCase() + "." + obj.lname.toLowerCase() + "@evolveu.ca");
 
     },
-    // makeEmailArr
     makeEmailArr: (arr) => {
         console.log(arr[0]);
         return (arr[0].toLowerCase() + '.' + arr[1].toLowerCase() + "@evolveu.ca");
-
-
         let button = document.getElementById("btn");
         let input = document.getElementById("inpt").value;
 
-        // comp 100
         function onButtonClicked() {
             console.log("I'am in the button click event");
         }
@@ -92,11 +66,9 @@ const functions = {
         }
 
         button.addEventListener("click", onButtonClicked);
-
-
     },
 
-    
+
 
 };
 
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,58 +1,23 @@
 import functions from './daily.js'
-import { tsThisType } from '@babel/types';
+import {
+    tsThisType
+} from '@babel/types';
 
 
-// *
-//     Sample data for the next few exercises.
-// */
-
-const data = {
-    staff: [
-        { fname: "Jane", lname: "Smith", balance: 10 },
-        { fname: "Liam", lname: "Henry", balance: 1000 },
-        { fname: "Emma", lname: "Jones", balance: 1330 },
-        { fname: "Olivia", lname: "Notly", balance: 310 },
-        { fname: "Noah", lname: "Ho", balance: 503 },
-        { fname: "William", lname: "Lee", balance: 520 },
-        { fname: "Benjamin", lname: "Amis", balance: 150 },
-    ],
-    company: "EvolveU",
-    city: "Calgary",
-    prov: "Alberta"
-};
-
-
-/*	
-	Write the function to build email addresses for the company.
-*/
-
-test('email builder for company', () => {
-    const staffEmail = functions.loopStaff(data.staff);
-
-/* 
-Test forEach
-*/
-// test('Advanced arrays forEach', () =>{
-//     expect(functions.makeForEach([newArray]))
-//         .toEqual([2,4,20,32])
-
-// }),
 
 /* 
 Test for of
 */
-//TODO:
-// test('Next level for of loop', () => {
-//     expect(functions.makeForOfLoop("cowSays"))
-//         .toEqual("m","o","o");
-// });
+test('Next level for of loop', () => {
+    expect(functions.makeForOfLoop())
+        .toEqual('United');
+});
 /* 
-Test for in
+Test for in loop
 */
-//FIXME:
 test('Next Level for in loop', () => {
-    expect(functions.makeForInLoop("Mark"))
-        .toEqual("Lawyer")
+    expect(functions.makeForInLoop("Age"))
+        .toEqual("Mark23Lawyer")
 });
 /* 
 Test do while
@@ -60,11 +25,11 @@ Test do while
 test('basic do while loop', () => {
     expect(functions.makeDoWhileLoop([6]))
         .toEqual(5);
-}); 
+});
 /* 
 Test while
 */
-test('basic while loop', () =>{
+test('basic while loop', () => {
     expect(functions.makeBasicWhileLoop([7]))
         .toEqual(6);
 });
