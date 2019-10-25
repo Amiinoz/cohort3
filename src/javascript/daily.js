0ab0935b25b9e6d2217228d6223821e4a3f5a826
Amiin <mohamedmagan@Amiins-MacBook-Pro.local>
2019-10-25 16:49:20 -0600

    Working the dailys

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,125 +1,133 @@
 import {
-    objectExpression
+  objectExpression,
+  arrayTypeAnnotation
 } from "@babel/types";
 
 const functions = {
-   
-    makeSlice: () => {
-        var fruits = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'];
-        var citrus = fruits.slice(1, 3);
-        return citrus;
-    },
+  loopStaff: (data) => {
+    const email = [];
+    for (let i = 0; i < data.length; i++){
+      // console.log(data[i]);
+     let staffEmail = functions.makeEmailObj(data[i]);
+    email.push(staffEmail);
+  } return email;
+  },
 
-    makeSplice: () => {
-        const array = [1, 2, 3, 4, 5];
-        return (array.splice(2));
-    },
+  makeSlice: () => {
+    var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+    var citrus = fruits.slice(1, 3);
+    return citrus;
+  },
 
-    makeForeach: () => {
-        const array = [1, 2, 10, 17];
-        const double = [];
-        const newArray = array.forEach((num) => {
-            double.push(num * 2);
-        })
-        return double;
+  makeSplice: () => {
+    const array = [1, 2, 3, 4, 5];
+    return array.splice(2);
+  },
 
-    },
+  makeForeach: () => {
+    const array = [1, 2, 10, 17];
+    const double = [];
+    const newArray = array.forEach(num => {
+      double.push(num * 2);
+    });
+    return double;
+  },
 
-    makeMap: (arr) => {
-        const newNums = [1, 2, 10, 16];
-        const mapNums = newNums.map(num => num * 2);
-        return mapNums;
-    },
+  makeMap: arr => {
+    const newNums = [1, 2, 10, 16];
+    const mapNums = newNums.map(num => num * 2);
+    return mapNums;
+  },
 
-    makeReduce: (arr) => {
-        var arr = [1, 2, 3, 4];
-        var sum = 0;
-        for (var i = 0; i < arr.length; i++) {
-            sum += arr[i];
-            return sum;
-        }
-    },
-
-    makeFilter: arr => {
-        const array = [3, 6, 9];
-        const filteredArray = array.filter(num => num < 5);
-        return filteredArray;
-    },
-
-    makeSort: arr => {
-        const months = ["March", "Jan", "Feb", "Dec"];
-        months.sort();
-        return months[0];
-    },
-
-    makeForOfLoop: arr => {
-        const airLines1 = ["United", "WestJet", "AirCanada"];
-        let item;
-        for (item of airLines1) {
-            return item;
-        }
-    },
-
-    makeForInLoop: (obj) => {
-        let person = {
-            name: "Mark",
-            Age: 23,
-            Occupation: "Lawyer"
-        };
-        let personalInfo = "";
-        let i;
-        for (i in person) {
-            personalInfo += person[i] + "";
-        }
-        return personalInfo;
-    },
-
-    makeDoWhileLoop: () => {
-        let counter = 5;
-        do {
-            return counter;
-            counter++;
-        } while (counter > 0);
-    },
-
-    makeBasicWhileLoop: () => {
-        let nums = 6;
-        while (nums < 7) {
-            return nums;
-            nums++;
-        }
-    },
-
-    makeBasicLoop: () => {
-        let array1 = [];
-        for (let i = 0; i <= 6; i++) {
-            array1.push(i);
-        }
-        return array1;
-    },
-
-    makeEmailObj: obj => {
-        return (
-            obj.fname.toLowerCase() + "." + obj.lname.toLowerCase() + "@evolveu.ca"
-        );
-    },
-
-    makeEmailArr: arr => {
-        console.log(arr[0]);
-        return arr[0].toLowerCase() + "." + arr[1].toLowerCase() + "@evolveu.ca";
-        let button = document.getElementById("btn");
-        let input = document.getElementById("inpt").value;
-
-        function onButtonClicked() {
-            console.log("I'am in the button click event");
-        }
-
-        function getValueFromInput() {
-            console.log(input);
-        }
-
-        button.addEventListener("click", onButtonClicked);
+  makeReduce: arr => {
+    var arr = [1, 2, 3, 4];
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+      return sum;
     }
+  },
+
+  makeFilter: arr => {
+    const array = [3, 6, 9];
+    const filteredArray = array.filter(num => num < 5);
+    return filteredArray;
+  },
+
+  makeSort: arr => {
+    const months = ["March", "Jan", "Feb", "Dec"];
+    months.sort();
+    return months[0];
+  },
+
+  makeForOfLoop: arr => {
+    const airLines1 = ["United", "WestJet", "AirCanada"];
+    let item;
+    for (item of airLines1) {
+      return item;
+    }
+  },
+
+  makeForInLoop: obj => {
+    let person = {
+      name: "Mark",
+      Age: 23,
+      Occupation: "Lawyer"
+    };
+    let personalInfo = "";
+    let i;
+    for (i in person) {
+      personalInfo += person[i] + "";
+    }
+    return personalInfo;
+  },
+
+  makeDoWhileLoop: () => {
+    let counter = 5;
+    do {
+      return counter;
+      counter++;
+    } while (counter > 0);
+  },
+
+  makeBasicWhileLoop: () => {
+    let nums = 6;
+    while (nums < 7) {
+      return nums;
+      nums++;
+    }
+  },
+
+  makeBasicLoop: () => {
+    let array1 = [];
+    for (let i = 0; i <= 6; i++) {
+      array1.push(i);
+    }
+    return array1;
+  },
+
+  makeEmailObj: obj => {
+    return (
+      obj.fname.toLowerCase() + "." + obj.lname.toLowerCase() + "@evolveu.ca"
+    );
+  },
+
+  makeEmailArr: arr => {
+    console.log(arr[0]);
+    return arr[0].toLowerCase() + "." + arr[1].toLowerCase() + "@evolveu.ca";
+    let button = document.getElementById("btn");
+    let input = document.getElementById("inpt").value;
+
+    function onButtonClicked() {
+      console.log("I'am in the button click event");
+    }
+
+    function getValueFromInput() {
+      console.log(input);
+    }
+
+    button.addEventListener("click", onButtonClicked);
+  }
 };
 
 export default functions;
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,103 +1,119 @@
 import functions from "./daily.js";
-import {
-    tsThisType
-} from "@babel/types";
+import { tsThisType } from "@babel/types";
 
-test('Make slice', () =>{
-    expect(functions.makeSlice()).toEqual(['Orange','Lemon']);
-})
+// test("email builder for company", () => {
+const data = {
+  staff: [
+      { fname: "Jane", lname: "Smith", balance: 10 },
+      { fname: "Liam", lname: "Henry", balance: 1000 },
+      { fname: "Emma", lname: "Jones", balance: 1330 },
+      { fname: "Olivia", lname: "Notly", balance: 310 },
+      { fname: "Noah", lname: "Ho", balance: 503 },
+      { fname: "William", lname: "Lee", balance: 520 },
+      { fname: "Benjamin", lname: "Amis", balance: 150 },
+  ],
+  company: "EvolveU",
+  city: "Calgary",
+  prov: "Alberta"
+};
 
-test('Splice', () => {
-    expect(functions.makeSplice()).toEqual([3, 4, 5]);
-})
-
-test('ForEach ', () => {
-    expect(functions.makeForeach()).toEqual([2, 4, 20, 34]);
+test('email builder for company', () => {
+  const staffEmail = functions.loopStaff(data.staff);
+  expect(staffEmail[0])
+      .toEqual("jane.smith@evolveu.ca");
+  expect(staffEmail[3])
+      .toEqual("olivia.notly@evolveu.ca");
+  expect(staffEmail[6])
+      .toEqual("benjamin.amis@evolveu.ca");
 });
 
-test('Make map()', () => {
-        expect(functions.makeMap()).toEqual([2, 4, 20, 32]);
+//   const staffEmail = functions.loopStaff(data.staff);
+//   expect(staffEmail[0]).toEqual("jane.smith@evolveu.ca");
+//   expect(staffEmail[3]).toEqual("olivia.notly@evolveu.ca");
+//   expect(staffEmail[6]).toEqual("benjamin.amis@evolveu.ca");
+// });
 
-    }),
+test("Make slice", () => {
+  expect(functions.makeSlice()).toEqual(["Orange", "Lemon"]);
+});
 
-    test("Make reduce()", () => {
-        expect(functions.makeReduce()).toEqual(1);
-    }),
-    test("Make filter()", () => {
-        expect(functions.makeFilter()).toEqual([3]);
-    });
+test("Splice", () => {
+  expect(functions.makeSplice()).toEqual([3, 4, 5]);
+});
+
+test("ForEach ", () => {
+  expect(functions.makeForeach()).toEqual([2, 4, 20, 34]);
+});
+
+test("Make map()", () => {
+  expect(functions.makeMap()).toEqual([2, 4, 20, 32]);
+}),
+  test("Make reduce()", () => {
+    expect(functions.makeReduce()).toEqual(1);
+  }),
+  test("Make filter()", () => {
+    expect(functions.makeFilter()).toEqual([3]);
+  });
 
 test("Make sort()", () => {
-        expect(functions.makeSort()).toEqual("Dec");
-    }),
+  expect(functions.makeSort()).toEqual("Dec");
+}),
+  test("Next level for of loop", () => {
+    expect(functions.makeForOfLoop()).toEqual("United");
+  });
 
-    test("Next level for of loop", () => {
-        expect(functions.makeForOfLoop()).toEqual("United");
-    });
-/* 
-Test for in loop
-*/
 test("Next Level for in loop", () => {
-    expect(functions.makeForInLoop("Age")).toEqual("Mark23Lawyer");
+  expect(functions.makeForInLoop("Age")).toEqual("Mark23Lawyer");
 });
-/* 
-Test do while
-*/
+
 test("basic do while loop", () => {
-    expect(functions.makeDoWhileLoop([6])).toEqual(5);
+  expect(functions.makeDoWhileLoop([6])).toEqual(5);
 });
-/* 
-Test while
-*/
+
 test("basic while loop", () => {
-    expect(functions.makeBasicWhileLoop([7])).toEqual(6);
+  expect(functions.makeBasicWhileLoop([7])).toEqual(6);
 });
-/* 
-Test for loop
-*/
+
 test("basic for loop", () => {
-    expect(functions.makeBasicLoop([0, 1, 2, 3, 4, 5, 6])).toEqual([
-        0,
-        1,
-        2,
-        3,
-        4,
-        5,
-        6
-    ]);
+  expect(functions.makeBasicLoop([0, 1, 2, 3, 4, 5, 6])).toEqual([
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+  ]);
 });
-/*
-Write the function to format an email based on an object / map
-*/
+
 test("email builder from an object / map", () => {
-    const name = {
-        fname: "first",
-        lname: "last"
-    };
-    expect(functions.makeEmailObj(name)).toEqual("first.last@evolveu.ca");
-    expect(
-        functions.makeEmailObj({
-            fname: "First",
-            lname: "Last"
-        })
-    ).toEqual("first.last@evolveu.ca");
-    expect(
-        functions.makeEmailObj({
-            fname: "Bill",
-            lname: "Smith"
-        })
-    ).toEqual("bill.smith@evolveu.ca");
+  const name = {
+    fname: "first",
+    lname: "last"
+  };
+  expect(functions.makeEmailObj(name)).toEqual("first.last@evolveu.ca");
+  expect(
+    functions.makeEmailObj({
+      fname: "First",
+      lname: "Last"
+    })
+  ).toEqual("first.last@evolveu.ca");
+  expect(
+    functions.makeEmailObj({
+      fname: "Bill",
+      lname: "Smith"
+    })
+  ).toEqual("bill.smith@evolveu.ca");
 });
-/*
-    Write a function to format an email based on an array.
-*/
+
 test("email builder from an array", () => {
-    const name = ["first", "last"];
-    expect(functions.makeEmailArr(name)).toEqual("first.last@evolveu.ca");
-    expect(functions.makeEmailArr(["First", "Last"])).toEqual(
-        "first.last@evolveu.ca"
-    );
-    expect(functions.makeEmailArr(["Bill", "Smith"])).toEqual(
-        "bill.smith@evolveu.ca"
-    );
+  const name = ["first", "last"];
+  expect(functions.makeEmailArr(name)).toEqual("first.last@evolveu.ca");
+  expect(functions.makeEmailArr(["First", "Last"])).toEqual(
+    "first.last@evolveu.ca"
+  );
+  expect(functions.makeEmailArr(["Bill", "Smith"])).toEqual(
+    "bill.smith@evolveu.ca"
+  );
 });
+// });
