9902b3277f71c8462430853aada64b8014eef89e
Amiin <mohamedmagan@Amiins-MacBook-Pro.local>
2019-11-12 09:13:09 -0700

    Updated 2019-11-12

--- src/01-getting-started/scripts/main.js
+++ src/01-getting-started/scripts/main.js
@@ -52,9 +52,46 @@ clear.addEventListener("click", () => {
 // My Tax Calculator End-----
 
 // My Working with Arrays start-----
+//FIXME:
+// add button
+// if the input = number then add to the array
+// if the input is not a number display a massage
+const addButton = document.getElementById("add");
+const arrayInput = document.getElementById("inp").value;
+const arrayShow = document.getElementById("show");
+const arrayToatal = document.getElementById("total");
+const arrayClear = document.getElementById("btn-clear");
+const ol = document.getElementById("arrayList");
 
-const add = document.getElementById("add");
-add.addEventListener("click", workArraysFunctions.addItems());
+addButton.addEventListener("click", newItem);
+let newArr = [];
+function newItem() {
+  // console.log("clicked");
+
+  if (typeof arrayInput === "number" && isNaN(arrayInput) !== true) {
+    newArr.push(arrayInput);
+    console.log(newArr);
+  } else {
+    return "now way hose";
+  }
+}
+
+//show button
+// will show the contents of the array.
+//  using the “State” concept What is the current “State” of the array
+// Show in a single string
+
+arrayShow.addEventListener("click", () => {});
+
+// Total button
+// will loop in the array and will return the total
+
+arrayToatal.addEventListener("click", () => {});
+
+// Clear button
+// will emty array
+
+arrayClear.addEventListener("clear", () => {});
 
 // My Working with Arrays end-----
 
