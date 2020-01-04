e8c86b53f6cdc2b79e613fae5cd6d63a7c5a3163
Amiinoz <momagan@gmail.com>
2020-01-03 20:04:48 -0700

    Working on React migration

--- src/react-02/src/components/accounts/Accounts.js
+++ src/react-02/src/components/accounts/Accounts.js
@@ -1,11 +1,57 @@
 import React from "react";
 
-function Accounts() {
-  return (
-    <div className='classAcc'>
-      <h2>Accounts</h2>
-    </div>
-  );
+class Accounts extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      accountName: " ",
+      accountType: " ",
+      startBalance: " "
+    };
+    this.onHandleChange = this.onHandleChange.bind(this);
+  }
+  onHandleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div className='classAcc'>
+        <h2>Accounts</h2>
+
+        <div className='classAccountCard'>
+          <h3>Account Info</h3>
+          <form>
+            <input
+              type='text'
+              name='accountName'
+              placeholder='accountName'
+              onChange={this.onHandleChange}
+            />
+            <h4>{this.state.accountName}</h4>
+            <input
+              type='text'
+              name='accountType'
+              placeholder='accountType'
+              onChange={this.onHandleChange}
+            />
+            <h4>{this.state.accountType}</h4>
+            <input
+              type='number'
+              name='startBalance'
+              placeholder='startBalance'
+              onChange={this.onHandleChange}
+            />
+            <h4>{this.state.startBalance}</h4>
+            <button type='button' className='btn'>
+              Enter
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Accounts;
